59d01e0ebef76ac64eb00734af65ae8c72d5c68a
yunyshs01 <jeoung09@naver.com>
2019-11-24 00:06:17 +0900

    Updates

---
+++ precache-manifest.d746adeaeb2832817ff56ffc474af179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db93de58057d6923210bed71f1607b0",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "636ff36550ebb6f97b1a",
+    "url": "/movie_app_2019/static/css/main.84569149.chunk.css"
+  },
+  {
+    "revision": "e4f1598052159155a1bf",
+    "url": "/movie_app_2019/static/js/2.a3b3a9a8.chunk.js"
+  },
+  {
+    "revision": "636ff36550ebb6f97b1a",
+    "url": "/movie_app_2019/static/js/main.afe9d28d.chunk.js"
+  },
+  {
+    "revision": "67dbae18500fdba58f01",
+    "url": "/movie_app_2019/static/js/runtime-main.9237056f.js"
+  }
+]);
