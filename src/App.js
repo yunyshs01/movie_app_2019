cef83f37ff88297fa2dd3c8346d79c3d9f1a3b26
yunyshs01 <jeoung09@naver.com>
2019-11-24 00:14:25 +0900

    slice summary thing and published

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   getMovies = async () => {
     const { data: { data: { movies } } } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    console.log(movies);
+    // console.log(movies);
     this.setState({ movies: movies, isLoading: false });
   }
 
@@ -25,15 +25,23 @@ class App extends React.Component {
     console.log("rendering");
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
-        ) : (<div class="movies">
+        ) : (<div className="movies">
           {movies.map(movie => {
-            console.log(movie);
-            return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+            //console.log(movie);
+            return <Movie
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres} />
+
           })
           }
         </div>
--- src/movie.js
+++ src/movie.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
+
+    
     return (
-        <div class="movie">
+        <div className="movie">
+            
             <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                {genres.map((genre, index)=>(
+                    <li key={index} className="genres__genre">{genre}</li>
+                ))}</ul>
+                <p className="movie__summary">{summary.slice(0,180)}</p>
                 
             </div>
 
-
         </div>
     )
-
-
 
 }
 
